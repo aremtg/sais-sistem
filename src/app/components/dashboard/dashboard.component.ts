271dfa8037313dd896470b0cb4aff713370b98fc
juan david montenegro <davidmontenegro-b@hotmail.com>
2025-12-23 09:54:30 -0500

    cambios

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuComponent } from "../../shared/menu/menu.component";
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet, FooterComponent],
+  imports: [MenuComponent, RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
