f9de3027333c2a797a59b0b1da4c6229f74c52c0
juan david montenegro <davidmontenegro-b@hotmail.com>
2025-12-23 09:35:38 -0500

    cabiois

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "../../shared/menu/menu.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent , RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
