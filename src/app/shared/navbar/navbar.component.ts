8b1b3e018189383a9c50a17369cde432b6256a15
juan david montenegro <davidmontenegro-b@hotmail.com>
2025-12-28 14:41:43 -0500

    cambios

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-
+import {MatIconModule} from '@angular/material/icon'
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, CommonModule, RouterLinkActive],
+  imports: [RouterLink, CommonModule, RouterLinkActive , MatIconModule ],
 
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
@@ -22,4 +22,29 @@ logout() {
     this.router.navigate(['/login']);
     console.log('Cerrando sesión...');
   }
+
+   dropdowns: { [key: string]: boolean } = {
+    usuarios: false,
+    headersuser:false
+    // puedes agregar más dropdowns aquí
+
+
+  };
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.relative')) {
+      this.dropdowns['headerUser'] = false;
+    }
+  }
+    toggleDropdown(key: string) {
+    // Cerrar otros dropdowns
+    Object.keys(this.dropdowns).forEach(k => {
+      if (k !== key) this.dropdowns[k] = false;
+    });
+    // Toggle el dropdown actual
+    this.dropdowns[key] = !this.dropdowns[key];
+  }
+
 }
