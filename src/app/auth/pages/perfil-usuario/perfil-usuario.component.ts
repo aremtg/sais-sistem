bf8e2520487da0f9b2264d98e015597f3a52e81b
juan david montenegro <davidmontenegro-b@hotmail.com>
2025-12-28 16:25:09 -0500

    CAMBIOS

--- src/app/auth/pages/perfil-usuario/perfil-usuario.component.ts
+++ src/app/auth/pages/perfil-usuario/perfil-usuario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-perfil-usuario',
   standalone: true,
-  imports: [],
+  imports: [ MatTooltipModule],
   templateUrl: './perfil-usuario.component.html',
   styleUrl: './perfil-usuario.component.scss'
 })
