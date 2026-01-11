a5040784afd27f96e0b08e192ebf6f6e04bf9c88
juan david montenegro <davidmontenegro-b@hotmail.com>
2026-01-10 20:05:12 -0500

    cambios

--- src/app/components/asistencia/registre-person/registre-person.component.ts
+++ src/app/components/asistencia/registre-person/registre-person.component.ts
@@ -3,22 +3,27 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-registre-person',
   standalone: true,
-  imports: [ CommonModule , ReactiveFormsModule, FormsModule , MatDialogModule , MatButtonModule],
+  imports: [ CommonModule , ReactiveFormsModule, FormsModule , MatDialogModule , MatButtonModule , MatIconModule],
   templateUrl: './registre-person.component.html',
   styleUrl: './registre-person.component.scss'
 })
 export class RegistrePersonComponent {
-   registerfrom! : FormGroup
+   registerfrom! : FormGroup;
+   mostrardetallesobjeto = false;
   constructor(registerfrom: FormBuilder) {
     this.registerfrom = registerfrom.group({
       cedula : ['' , Validators.required],
       objeto : [''],
     })
 
+  }
+  mostarobsjeto(){
+    this.mostrardetallesobjeto = true;
   }
 
 }
