33fd87940b0135d08b744f30d0fe3d936ac97f6e
juan david montenegro <davidmontenegro-b@hotmail.com>
2026-02-21 10:31:33 -0500

    cambios

--- src/app/components/teacher/registerteacher/registerteacher.component.ts
+++ src/app/components/teacher/registerteacher/registerteacher.component.ts
@@ -1,11 +1,53 @@
+import { DialogModule } from '@angular/cdk/dialog';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TecaherService } from '../service/tecaher.service';
+import { MatDialogRef, MatDialogTitle, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-registerteacher',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, DialogModule, FormsModule, MatButtonModule, MatDialogTitle, MatDialogActions, MatDialogContent],
   templateUrl: './registerteacher.component.html',
 })
 export class RegisterteacherComponent {
-
+  registerTeacher : FormGroup;
+  constructor( private tecaherService : TecaherService ,
+    private dialog : MatDialogRef<RegisterteacherComponent> ,
+    private fb : FormBuilder ,
+    private  snackbar : MatSnackBar
+  ) {
+    this.registerTeacher = this.fb.group({
+      cedula : ['' , Validators.required ],
+      nombre : ['', Validators.required ],
+      apellido : ['', Validators.required],
+      profesion : ['', Validators.required],
+      rama : ['', Validators.required],
+      jornada : ['', Validators.required],
+      email : ['' , [Validators.required , Validators.email ]],
+      telefono : ['', Validators.required]
+    });
+   }
+    cancelar(){
+      this.registerTeacher.reset();
+      this.dialog.close();
+    }
+    guardar(){
+      if (this.registerTeacher.invalid) {
+        this.snackbar.open('Por favor completa todos los campos', 'Cerrar', { duration: 2000 });
+        return;
+      }
+      this.tecaherService.createTeacher(this.registerTeacher.value).subscribe({
+        next: (response) => {
+          this.snackbar.open(response.message, 'Cerrar', { duration: 2000 });
+          this.dialog.close();
+        },
+        error: (error) => {
+          this.snackbar.open(error.message, 'Cerrar', { duration: 2000 });
+        }
+      });
+    }
 }
