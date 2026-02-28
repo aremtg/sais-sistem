3f8d7149de7f381de27419340b7ac3c0f268723d
juan david montenegro <davidmontenegro-b@hotmail.com>
2026-02-28 08:56:02 -0500

    cambios

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -1,12 +1,19 @@
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [ MatTooltipModule],
+  imports: [ MatTooltipModule , MatIconModule , ReactiveFormsModule , CommonModule , FormsModule , DialogModule],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
 export class CursosComponent {
+  constructor( private dialog : Dialog){}
+
+  registrocursos(){}
 
 }
